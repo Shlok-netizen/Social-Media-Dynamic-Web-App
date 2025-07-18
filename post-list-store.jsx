c223efad96c36601e90fd8ac02902433c4f76320
Shlok-netizen <shloksinghshloksingh18@gmail.com>
2025-07-18 18:06:25 +0530

    Add files via upload

---
+++ post-list-store.jsx
@@ -0,0 +1,77 @@
+import { createContext } from "react";
+import { useReducer } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  }else if(action.type === "ADD_POST"){
+    newPostList = [action.payload,...currPostList]
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = (userId, postTitle, postBody, reactions, tags) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      payload: {
+        id: Date.now(),
+        title: postTitle,
+        body: postBody,
+        reaction: reactions,
+        userId: userId,
+        tags: tags,
+      },
+    });
+  };
+
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
+
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Vrindavan",
+    body: "Hi Freind's I am going to the vrindavan today . Hope to enjoying a lot",
+    reaction: 2,
+    userId: "user-9",
+    tags: ["Vrindavan", "Mathura", "Barsana"],
+  },
+  {
+    id: "2",
+    title: "I am Pass",
+    body: "Hi Freind's I am passing in Btech . Hope to enjoying a lot",
+    reaction: 15,
+    userId: "user-12",
+    tags: ["Graduating", "Unbelievable"],
+  },
+];
+
+export default PostListProvider;
